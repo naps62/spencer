54205a0cb26634743d18e4fe9c27f2ff65caa8dd
Miguel Palhas <mpalhas@gmail.com>
2014-10-21 15:44:19 +0100

    Adding secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spencer::Application.config.secret_key_base = 'b2478c46fbd829702905d204cea8e239e04925f085e7496294475bd21de51f9418dbdba96995f73cb0c9e20174a29626d0d1316373b34388ea5b2bdb740dab88'
